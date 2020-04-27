de1abb0548195f55b4f5d20c9f1aec32a711d168
kate smith <kateesmith4994@gmail.com>
2020-04-27 17:12:13 +0100

    adding files worked on

---
+++ boris_bike.rb
@@ -0,0 +1,16 @@
+# As a user,
+# So I can find customers,
+# I want to search for my customers by their surnames.
+
+class Person
+  def initialize
+    @name = ['name1',
+      'name2',
+      'name3'
+    ]
+  end
+
+  def search_for_surname
+    # return "bob"
+  end
+end
---
+++ boris_bike_spec.rb
@@ -0,0 +1,10 @@
+require 'boris_bike.rb'
+require 'rspec'
+
+#function to test that we search by surnames
+describe Person do
+  it 'returns customer when we search their surname' do
+    person_test = Person.new
+    expect(person_test.search_for_surname).to eq("smith")
+  end
+end
