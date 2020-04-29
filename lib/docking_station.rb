10f77280d7520b5d5b448e1a6afe36b70c6c1916
kate smith <kateesmith4994@gmail.com>
2020-04-29 16:44:50 +0100

    end of wednesday pairing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,30 @@
 require_relative 'bike'
 class DockingStation
-  def release_bike
+
+  def initialize
+    @number_of_bikes = 0
+  end
+
+  def bike_released
     bike = Bike.new
   end
 
-  def dock(add_bike)
-    total = 0
-    total += add_bike
-    return total
+  def release_bike
+    if @number_of_bikes < 1
+      return "There are no bikes here."
+    else
+      @number_of_bikes -= 1
+      bike_released
+    end
   end
 
+  def dock(add_bike = 0)
+    @number_of_bikes += add_bike
+    return "There are #{@number_of_bikes} bike(s) in the docking station"
+  end
 
 end
+
+# london = DockingStation.new
+# london.dock(2)
+# puts london.dock
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,17 +4,23 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = subject.bike_released
     expect(bike).to be_working
   end
 
-  it 'adds one bike to bike count in docking_station' do
-    expect(subject.dock(1)).to eq(1)
+  it 'does not release a bike if there is less than 1 in the docking station' do
+    expect(subject.release_bike).to eq("There are no bikes here.")
   end
 
-  it 'returns the number of bikes in dock' do
-    subject.dock(1)
-    subject.dock(2)
-    expect(subject.dock(1)).to eq(4)
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'adds one bike to bike count in docking_station' do
+    expect(subject.dock(1)).to eq("There are 1 bike(s) in the docking station")
   end
+
+  # it 'returns the number of bikes in dock' do
+  #   subject.dock(1)
+  #   subject.dock(2)
+  #   expect(subject.dock(1)).to eq(4)
+  # end
 end
